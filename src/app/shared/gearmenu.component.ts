a7559cace35f4916c39e035af3381e77d84457f4
Tim Coker <cokert@gmail.com>
2017-01-18 23:24:05 -0500

    got menu to shift left instead of expanding as mentioned in issue #1

--- src/app/shared/gearmenu.component.ts
+++ src/app/shared/gearmenu.component.ts
@@ -2,25 +2,33 @@ import { Component,
          OnInit,
          Input } from '@angular/core';
 
+declare var $:any;
+
 @Component({
   selector: 'app-gear-menu',
   template: `
     <span class="dropdown">
-      <a aria-haspopup="true" class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">
-           <span class="glyphicon glyphicon-cog"></span>
-           <!--<span class="caret"></span>-->
+      <a aria-haspopup="true"
+         class="dropdown-toggle"
+         role="button"
+         (click)="showMenu($event)">
+       <span class="glyphicon glyphicon-cog"></span>
       </a>
-      <ul class="dropdown-menu">
+      <ul class="dropdown-menu gear-menu">
         <li [attr.data-toggle]="i.data_toggle ? i.data_toggle : ''"
             [attr.data-target]="i.data_target ? i.data_target : ''"
             (click)="performAction(i.action);"
             *ngFor="let i of menuData" role="button">
-          <a class="hand">
+          <a>
             {{ i.label }}
           </a>
         </li>
       </ul>
-  </span>`
+  </span>`,
+  styles: [`
+    .edge {
+      right: 0px;
+    }`]
 })
 export class GearMenuComponent implements OnInit {
   @Input() menuData = null;
@@ -28,6 +36,35 @@ export class GearMenuComponent implements OnInit {
   constructor() { console.log('gearmenu'); }
 
   ngOnInit() { }
+
+  showMenu(event) {
+    var gearWidth = $(event.target).width();
+    var elem = $(event.target).parent().parent().children('ul');
+
+    if (elem.is(':visible')) {
+      elem.hide();
+      return;
+    }
+
+    elem.show();
+
+    var left = event.clientX;
+    var width = elem.width();
+
+    var offset = -width + gearWidth;
+
+    var docWidth = $(".container").width();
+
+    var isEntirelyVisible = (left + width <= docWidth);
+
+    if (!isEntirelyVisible) {
+      console.log("shifting");
+      elem.css({left: offset + 'px'});
+    } else {
+      console.log("NOT shifting");
+      elem.css({left: '0px'});
+    }
+  }
 
   performAction(action) {
     if (!action) return;
